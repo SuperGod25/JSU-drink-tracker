6e84d706ab3393a2fd8f6e6fadd1828bdebfaeee
Sebastian Ionel Filipovici <Sebastian-Ionel.Filipovici@endava.com>
2025-07-21 10:08:43 +0300

    created the server file for supporting the service

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.ts or server.js
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// SPA fallback for client-side routing
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
